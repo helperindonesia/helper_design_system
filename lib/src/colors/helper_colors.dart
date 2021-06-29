d0b7e1320e2d1a783a12543b124abbff26bf46c3
Afdal <Afdaldev@gmail.com>
2021-06-29 15:56:49 +0800

    add color value

--- lib/src/colors/helper_colors.dart
+++ lib/src/colors/helper_colors.dart
@@ -5,14 +5,19 @@ class HelperColors {
   static const Color black = Color(0xFF313131);
   static const Color white = Color(0xFFFFFFFF);
 
-  static const Color green = Color(0xFF28A745);
+  static const Color green = Color(0xFF48B759);
   static const Color red = Color(0xFFD93B3B);
   static const Color blue = Color(0xFF0069D9);
 
-  static const Color orange2 = Color(0xFFFE7104);
+  static const Color orange2 = Color(0xFFFE7F1D);
   static const Color orange3 = Color(0xFFFE8D36);
-  static const Color orange4 = Color(0xFFFE7104);
-  static const Color orange5 = Color(0xFFFE7104);
+  static const Color orange4 = Color(0xFFFE9B4F);
+  static const Color orange5 = Color(0xFFFEA968);
+  static const Color orange6 = Color(0xFFFEB781);
+  static const Color orange7 = Color(0xFFFEC69A);
+  static const Color orange8 = Color(0xFFFED4B3);
+  static const Color orange9 = Color(0xFFFEE2CC);
+  static const Color orange10 = Color(0xFFFEF0E5);
 
   static const Color black2 = Color(0xFF454545);
   static const Color black3 = Color(0xFF5A5A5A);
@@ -24,18 +29,18 @@ class HelperColors {
   static const Color black9 = Color(0xFFD5D5D5);
   static const Color black10 = Color(0xFFEAEAEA);
 
-  static const Color green2 = Color(0xFF28A745);
-  static const Color green3 = Color(0xFF28A745);
-  static const Color green4 = Color(0xFF28A745);
-  static const Color green5 = Color(0xFF28A745);
+  static const Color green2 = Color(0xFF5ABE69);
+  static const Color green3 = Color(0xFF6CC57A);
+  static const Color green4 = Color(0xFF7ECC8A);
+  static const Color green5 = Color(0xFF91D39B);
 
-  static const Color red2 = Color(0xFFD93B3B);
-  static const Color red3 = Color(0xFFD93B3B);
-  static const Color red4 = Color(0xFFD93B3B);
-  static const Color red5 = Color(0xFFD93B3B);
+  static const Color red2 = Color(0xFFDC4E4E);
+  static const Color red3 = Color(0xFFE06262);
+  static const Color red4 = Color(0xFFE47575);
+  static const Color red5 = Color(0xFFE88989);
 
-  static const Color blue2 = Color(0xFF0069D9);
-  static const Color blue3 = Color(0xFF0069D9);
-  static const Color blue4 = Color(0xFF0069D9);
-  static const Color blue5 = Color(0xFF0069D9);
+  static const Color blue2 = Color(0xFF1978DC);
+  static const Color blue3 = Color(0xFF3387E0);
+  static const Color blue4 = Color(0xFF4C96E4);
+  static const Color blue5 = Color(0xFF66A5E8);
 }
