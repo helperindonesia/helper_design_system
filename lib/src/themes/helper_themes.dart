fbfb8f50f439b68189903b687a5a0ffafda72529
Afdal <Afdaldev@gmail.com>
2021-06-29 15:56:58 +0800

    change typescale

--- lib/src/themes/helper_themes.dart
+++ lib/src/themes/helper_themes.dart
@@ -16,16 +16,20 @@ class HelperThemeData {
         fontSize: 28, fontWeight: FontWeight.bold, letterSpacing: 0.25),
     headline3: TextStyle(
         fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 0.25),
+    headline4: TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 0.25),
+    headline5: TextStyle(
+        fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: 0.25),
     subtitle1: TextStyle(
         fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: 0.25),
     subtitle2: TextStyle(
         fontSize: 16, fontWeight: FontWeight.bold, letterSpacing: 0.25),
     bodyText1: TextStyle(
-        fontSize: 14, fontWeight: FontWeight.normal, letterSpacing: 0.15),
+        fontSize: 16, fontWeight: FontWeight.w600, letterSpacing: 0.1),
+    bodyText2: TextStyle(
+        fontSize: 16, fontWeight: FontWeight.normal, letterSpacing: 0.1),
     caption: TextStyle(
         fontSize: 12, fontWeight: FontWeight.normal, letterSpacing: 0.4),
-    bodyText2: TextStyle(
-        fontSize: 10, fontWeight: FontWeight.normal, letterSpacing: 0.4),
     button: TextStyle(
         fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 1.0),
   );
