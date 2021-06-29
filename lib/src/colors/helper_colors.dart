0472273a82c12ddae5066ba6981cd9a99adc3b78
Afdal <Afdaldev@gmail.com>
2021-06-29 11:10:23 +0800

    add monochromo color

--- lib/src/colors/helper_colors.dart
+++ lib/src/colors/helper_colors.dart
@@ -10,19 +10,19 @@ class HelperColors {
   static const Color blue = Color(0xFF0069D9);
 
   static const Color orange2 = Color(0xFFFE7104);
-  static const Color orange3 = Color(0xFFFE7104);
+  static const Color orange3 = Color(0xFFFE8D36);
   static const Color orange4 = Color(0xFFFE7104);
   static const Color orange5 = Color(0xFFFE7104);
 
-  static const Color gray2 = Color(0xFFC1C1C1);
-  static const Color gray3 = Color(0xFFACACAC);
-  static const Color gray4 = Color(0xFFD5D5D5);
-  static const Color gray5 = Color(0xFFFE7104);
-
-  static const Color black2 = Color(0xFF313131);
-  static const Color black3 = Color(0xFF313131);
-  static const Color black4 = Color(0xFF313131);
-  static const Color black5 = Color(0xFF313131);
+  static const Color black2 = Color(0xFF454545);
+  static const Color black3 = Color(0xFF5A5A5A);
+  static const Color black4 = Color(0xFF6E6E6E);
+  static const Color black5 = Color(0xFF838383);
+  static const Color black6 = Color(0xFF979797);
+  static const Color black7 = Color(0xFFACACAC);
+  static const Color black8 = Color(0xFFC1C1C1);
+  static const Color black9 = Color(0xFFD5D5D5);
+  static const Color black10 = Color(0xFFEAEAEA);
 
   static const Color green2 = Color(0xFF28A745);
   static const Color green3 = Color(0xFF28A745);
