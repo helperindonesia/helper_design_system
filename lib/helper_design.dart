ef583c4cfbebec0b13d4853d07d479f3f925f060
Afdal <Afdaldev@gmail.com>
2021-07-01 00:33:31 +0800

    fix: optimize exports

--- lib/helper_design.dart
+++ lib/helper_design.dart
@@ -1,6 +1,6 @@
+library helper_design;
+
 export 'src/colors/colors.dart';
 export 'src/icons/icons.dart';
 export 'src/themes/themes.dart';
-export 'src/widgets/button/buttons.dart';
-export 'src/widgets/textfield/textfield.dart';
-export 'src/widgets/expand/expand.dart';
+export 'src/widgets/widgets.dart';
