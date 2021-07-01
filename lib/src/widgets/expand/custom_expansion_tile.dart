a09a6fee260945f2b9d0e31bee068770406df54b
Afdal <Afdaldev@gmail.com>
2021-07-01 21:44:26 +0800

    refactor : custom expansion tile

---
+++ lib/src/widgets/expand/custom_expansion_tile.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:helper_design/helper_design.dart';
+
+const Duration _kExpand = Duration(milliseconds: 200);
+
+class CustomExpansionView extends StatefulWidget {
+  const CustomExpansionView({
+    Key? key,
+    required this.title,
+    this.onExpansionChanged,
+    this.children = const <Widget>[],
+    this.trailing,
+    this.initiallyExpanded = false,
+    this.maintainState = false,
+    this.expandedCrossAxisAlignment,
+    this.expandedAlignment,
+    this.childrenPadding,
+    this.backgroundColor,
+    this.collapsedBackgroundColor,
+    this.iconColor,
+    this.iconSize,
+    this.titleStyle,
+    this.titlePadding,
+  }) : super(key: key);
+
+  final String title;
+  final ValueChanged<bool>? onExpansionChanged;
+  final List<Widget> children;
+  final Color? backgroundColor;
+  final Color? collapsedBackgroundColor;
+  final Widget? trailing;
+  final bool initiallyExpanded;
+  final bool maintainState;
+  final Alignment? expandedAlignment;
+  final CrossAxisAlignment? expandedCrossAxisAlignment;
+  final EdgeInsetsGeometry? childrenPadding;
+  final Color? iconColor;
+  final double? iconSize;
+  final TextStyle? titleStyle;
+  final EdgeInsetsGeometry? titlePadding;
+
+  @override
+  _CustomExpansionViewState createState() => _CustomExpansionViewState();
+}
+
+class _CustomExpansionViewState extends State<CustomExpansionView>
+    with SingleTickerProviderStateMixin {
+  static final Animatable<double> _easeOutTween =
+      CurveTween(curve: Curves.easeOut);
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.easeIn);
+  static final Animatable<double> _halfTween =
+      Tween<double>(begin: 0.0, end: 0.5);
+
+  final ColorTween _backgroundColorTween = ColorTween();
+
+  late AnimationController _controller;
+  late Animation<double> _iconTurns;
+  late Animation<double> _heightFactor;
+  late Animation<Color?> _backgroundColor;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: _kExpand, vsync: this);
+    _heightFactor = _controller.drive(_easeInTween);
+    _iconTurns = _controller.drive(_halfTween.chain(_easeInTween));
+    _backgroundColor =
+        _controller.drive(_backgroundColorTween.chain(_easeOutTween));
+
+    _isExpanded = PageStorage.of(context)?.readState(context) as bool? ??
+        widget.initiallyExpanded;
+    if (_isExpanded) _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse().then<void>((void value) {
+          if (!mounted) return;
+          setState(() {
+            // Rebuild without widget.children.
+          });
+        });
+      }
+      PageStorage.of(context)?.writeState(context, _isExpanded);
+    });
+    widget.onExpansionChanged?.call(_isExpanded);
+  }
+
+  Widget _buildChildren(BuildContext context, Widget? child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: _backgroundColor.value ?? HelperColors.white,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          InkWell(
+            onTap: _handleTap,
+            child: Container(
+              padding: widget.titlePadding ??
+                  EdgeInsets.only(left: 16.0, top: 12, bottom: 12, right: 12.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                      child: Text(
+                    widget.title,
+                    style: widget.titleStyle ??
+                        HelperThemeData.textTheme.caption
+                            ?.copyWith(color: HelperColors.black5),
+                  )),
+                  widget.trailing ??
+                      RotationTransition(
+                        turns: _iconTurns,
+                        child: Icon(
+                          Icons.expand_more,
+                          size: widget.iconSize ?? 24,
+                          color: widget.iconColor ?? HelperColors.black8,
+                        ),
+                      ),
+                ],
+              ),
+            ),
+          ),
+          ClipRect(
+            child: Align(
+              alignment: widget.expandedAlignment ?? Alignment.center,
+              heightFactor: _heightFactor.value,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    _backgroundColorTween
+      ..begin = widget.collapsedBackgroundColor
+      ..end = widget.backgroundColor ?? HelperColors.white;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool closed = !_isExpanded && _controller.isDismissed;
+    final bool shouldRemoveChildren = closed && !widget.maintainState;
+
+    final Widget result = Offstage(
+      child: TickerMode(
+        child: Padding(
+          padding: widget.childrenPadding ?? EdgeInsets.fromLTRB(16, 0, 12, 16),
+          child: Column(
+            crossAxisAlignment:
+                widget.expandedCrossAxisAlignment ?? CrossAxisAlignment.center,
+            children: widget.children,
+          ),
+        ),
+        enabled: !closed,
+      ),
+      offstage: closed,
+    );
+
+    return AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: shouldRemoveChildren ? null : result,
+    );
+  }
+}
