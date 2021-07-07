dc90513708c885f9e92b0122183a684ab7098617
Afdal <Afdaldev@gmail.com>
2021-07-07 12:31:02 +0800

    refactor(helper_app_bar): add factory with help icon
    this is add factory with help icon (can change leading icon and replace actions to Sizedbox)

--- lib/src/widgets/appbar/helper_app_bar.dart
+++ lib/src/widgets/appbar/helper_app_bar.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:helper_design/helper_design.dart';
 
 class HelperAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String? title;
+  final Widget? title;
   final TextStyle? textStyle;
-
-  final IconData? leading;
+  final Widget? leading;
   final double? leadingSize;
-
-  final VoidCallback? onLeadingPressed;
-  final VoidCallback? onHelpPressed;
+  final VoidCallback? onPressed;
   final List<Widget>? actions;
   final double? elevation;
   final Color? backgroundColor;
-  final bool isHelp;
 
   const HelperAppBar({
     Key? key,
@@ -21,53 +17,82 @@ class HelperAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.textStyle,
     this.leading,
     this.leadingSize,
-    this.onLeadingPressed,
-    this.onHelpPressed,
+    this.onPressed,
     this.actions,
     this.elevation,
     this.backgroundColor,
-    this.isHelp = false,
   }) : super(key: key);
+
+  factory HelperAppBar.helpIcon({
+    String? title,
+    TextStyle? textStyle,
+    IconData? leadingIcon,
+    double? leadingSize,
+    VoidCallback? onPressed,
+    VoidCallback? onHelpPressed,
+    List<Widget>? actions,
+    double? elevation,
+    Color? backgroundColor,
+  }) = _HelperAppBarWithHelpIcon;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      shadowColor: HelperColors.black,
       titleSpacing: 0.0,
-      leading: InkWell(
-        onTap: onLeadingPressed,
-        child: Icon(
-          leading ?? Icons.arrow_back,
-          color: HelperColors.black3,
-          size: leadingSize ?? 24.0,
-        ),
-      ),
-      title: Text(
-        title ?? '',
-        style: textStyle ??
-            HelperThemeData.textTheme.headline4!
-                .copyWith(color: HelperColors.black),
-      ),
+      leading: leading,
+      title: title,
       centerTitle: false,
-      actions: isHelp
-          ? <Widget>[
-              InkWell(
-                onTap: onHelpPressed,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Icon(
-                    Icons.help,
-                    color: HelperColors.black3,
-                    size: 24.0,
-                  ),
-                ),
-              )
-            ]
-          : actions,
-      elevation: elevation ?? 4/100,
+      actions: actions,
+      elevation: elevation ?? 4 / 100,
       backgroundColor: backgroundColor ?? HelperColors.white,
     );
   }
 
   @override
   Size get preferredSize => Size.fromHeight(56);
+}
+
+class _HelperAppBarWithHelpIcon extends HelperAppBar {
+  _HelperAppBarWithHelpIcon(
+      {Key? key,
+      String? title,
+      TextStyle? textStyle,
+      IconData? leadingIcon,
+      double? leadingSize,
+      VoidCallback? onPressed,
+      VoidCallback? onHelpPressed,
+      List<Widget>? actions,
+      double? elevation,
+      Color? backgroundColor})
+      : super(
+            key: key,
+            leading: IconButton(
+              onPressed: onPressed,
+              icon: Icon(
+                leadingIcon ?? Icons.arrow_back,
+                color: HelperColors.black3,
+              ),
+              iconSize: leadingSize ?? 24.0,
+            ),
+            title: Text(
+              title ?? '',
+              style: textStyle ??
+                  HelperThemeData.textTheme.headline4!
+                      .copyWith(color: HelperColors.black),
+            ),
+            actions: actions ??
+                <Widget>[
+                  IconButton(
+                    padding: EdgeInsets.only(right: 16.0),
+                    onPressed: onHelpPressed,
+                    icon: Icon(
+                      Icons.help,
+                      color: HelperColors.black3,
+                    ),
+                    iconSize: 24.0,
+                  )
+                ],
+            elevation: elevation,
+            backgroundColor: backgroundColor);
 }
