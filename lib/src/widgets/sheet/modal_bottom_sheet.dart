088f28e37bf1fa5ca9683bc3f321cda7aaf86887
Afdal <Afdaldev@gmail.com>
2021-07-28 18:14:12 +0800

    refactor(modal_bottom_sheet): fix overflow from bottom sheet
    this is fix overflow in modal bottom sheet. because, default height max value of bottom sheet is half of screen

--- lib/src/widgets/sheet/modal_bottom_sheet.dart
+++ lib/src/widgets/sheet/modal_bottom_sheet.dart
@@ -11,39 +11,41 @@ class ModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: BaseButton(
-              onPressed: () => Navigator.pop(context),
-              backgroundColor: backgroundColor ?? HelperColors.white,
-              height: 40.0,
-              width: 40.0,
-              child: Icon(
-                Icons.close_rounded,
-                size: 24.0,
-                color: HelperColors.black3,
+    return Wrap(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: BaseButton(
+                onPressed: () => Navigator.pop(context),
+                backgroundColor: backgroundColor ?? HelperColors.white,
+                height: 40.0,
+                width: 40.0,
+                child: Icon(
+                  Icons.close_rounded,
+                  size: 24.0,
+                  color: HelperColors.black3,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 16.0),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: backgroundColor ?? HelperColors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(16.0),
+            SizedBox(height: 16.0),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: backgroundColor ?? HelperColors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(16.0),
+                ),
+              ),
+              child: Column(
+                children: children,
               ),
             ),
-            child: Column(
-              children: children,
-            ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
