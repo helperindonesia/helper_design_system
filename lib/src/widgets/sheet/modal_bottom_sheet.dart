6dc9224936051bb90c83a3815480434b86f85b32
Afdal <Afdaldev@gmail.com>
2021-07-29 19:56:20 +0800

    feat(modal_bottom_sheet): add multiple button, optional iconData
    this is add multiple button, iconData can be change and handle button press

--- lib/src/widgets/sheet/modal_bottom_sheet.dart
+++ lib/src/widgets/sheet/modal_bottom_sheet.dart
@@ -4,30 +4,62 @@ import 'package:helper_design/helper_design.dart';
 class ModalBottomSheet extends StatelessWidget {
   final Color? backgroundColor;
   final List<Widget> children;
+  final bool multipleButtonOnTop;
+  final IconData? firstIcon;
+  final IconData? secondIcon;
+  final VoidCallback? firstButtonPress;
+  final VoidCallback? secondButtonPress;
 
-  const ModalBottomSheet(
-      {Key? key, this.backgroundColor, required this.children})
-      : super(key: key);
+  const ModalBottomSheet({
+    Key? key,
+    this.backgroundColor,
+    required this.children,
+    this.multipleButtonOnTop = false,
+    this.firstIcon,
+    this.secondIcon,
+    this.firstButtonPress,
+    this.secondButtonPress,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
       children: [
         Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: BaseButton(
-                onPressed: () => Navigator.pop(context),
-                backgroundColor: backgroundColor ?? HelperColors.white,
-                height: 40.0,
-                width: 40.0,
-                child: Icon(
-                  Icons.close_rounded,
-                  size: 24.0,
-                  color: HelperColors.black3,
-                ),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: multipleButtonOnTop
+                    ? MainAxisAlignment.spaceBetween
+                    : MainAxisAlignment.end,
+                children: [
+                  BaseButton(
+                    onPressed: firstButtonPress ?? () => Navigator.pop(context),
+                    backgroundColor: backgroundColor ?? HelperColors.white,
+                    height: 40.0,
+                    width: 40.0,
+                    child: Icon(
+                      firstIcon ?? Icons.close_rounded,
+                      size: 24.0,
+                      color: HelperColors.black3,
+                    ),
+                  ),
+                  multipleButtonOnTop
+                      ? BaseButton(
+                          onPressed: secondButtonPress ?? () => () {},
+                          backgroundColor:
+                              backgroundColor ?? HelperColors.white,
+                          height: 40.0,
+                          width: 40.0,
+                          child: Icon(
+                            secondIcon ?? Icons.my_location_rounded,
+                            size: 24.0,
+                            color: HelperColors.black3,
+                          ),
+                        )
+                      : SizedBox(),
+                ],
               ),
             ),
             SizedBox(height: 16.0),
