681da04ed2b9a5e0c310678ab78d98674531b863
Afdal <Afdaldev@gmail.com>
2021-08-13 12:12:40 +0800

    test(button_test): add more test

--- test/component/button/button_test.dart
+++ test/component/button/button_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide OutlinedButton;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:helper_design/helper_design.dart';
 
@@ -34,17 +34,83 @@ void main() {
   });
 
   group('Circle Icon Button Test', () {
+    bool? _value;
+    Key key = Key('Circle Icon Button');
     testWidgets('Circle Icon Button', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home:
-              Scaffold(body: CircleIconButton(onPressed: () {}, badgeCount: 1)),
+          home: Scaffold(
+              body: CircleIconButton(
+                  key: key,
+                  onPressed: () {
+                    _value = true;
+                  },
+                  badgeCount: 1)),
         ),
       );
 
       //Verify Widget
+      expect(_value, isNull);
       expect(find.byIcon(Icons.chat_bubble_rounded), findsOneWidget);
       expect(find.text('1'), findsOneWidget);
+
+      //Tap Circle Icon Button
+      await tester.tap(find.byKey(key));
+
+      //Verify Circle Icon Button after tap
+      expect(_value, isTrue);
+    });
+  });
+
+  group('Primary Button', () {
+    bool? _value;
+    testWidgets('Primary Button', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: PrimaryButton(
+            onPressed: () {
+              _value = true;
+            },
+            text: 'Primary Button',
+          ),
+        ),
+      ));
+
+      //Verify Widget
+      expect(_value, isNull);
+      expect(find.text('Primary Button'), findsOneWidget);
+
+      //Tap primary button
+      await tester.tap(find.text('Primary Button'));
+
+      //Verify after tap Primary Button
+      expect(_value, isTrue);
+    });
+  });
+
+  group('Outlined Button', () {
+    bool? _value;
+    testWidgets('Outlined Button', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: OutlinedButton(
+            text: 'Outlined Button',
+            onPressed: () {
+              _value = true;
+            },
+          ),
+        ),
+      ));
+
+      //Verify Widget
+      expect(_value, isNull);
+      expect(find.text('Outlined Button'), findsOneWidget);
+
+      //Tap Outlined Button
+      await tester.tap(find.text('Outlined Button'));
+
+      //Verify Outlined Button after tap
+      expect(_value, isTrue);
     });
   });
 }
