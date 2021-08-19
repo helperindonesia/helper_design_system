df365241a126cacc6b357bd23af00b816d33231e
Afdal <Afdaldev@gmail.com>
2021-08-19 16:43:02 +0800

    refactor(order_finish_status): change old label to new label

--- example/lib/order/common/order_finish_status.dart
+++ example/lib/order/common/order_finish_status.dart
@@ -13,49 +13,48 @@ class OrderFinishStatus extends StatelessWidget {
     return Container(
       child: Column(
         children: [
-          Row(
-            children: [
-              SizedBox(width: 16.0),
-              Image.asset(
-                'assets/images/ic_helper.webp',
-                height: 40.0,
-                width: 40.0,
-              ),
-              SizedBox(width: 16.0),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/ic_helper.webp',
+                  height: 40.0,
+                  width: 40.0,
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Helper Asisten',
+                        style: HelperThemeData.textTheme.headline5,
+                      ),
+                      Text(
+                        'Tawaran Helpermu',
+                        style: HelperThemeData.textTheme.buttonText1!
+                            .copyWith(fontSize: 12.0, color: HelperColors.black5),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Text(
-                      'Helper Asisten',
-                      style: HelperThemeData.textTheme.headline5,
-                    ),
-                    Text(
-                      'Tawaran Helpermu',
-                      style: HelperThemeData.textTheme.buttonText1!
-                          .copyWith(fontSize: 12.0, color: HelperColors.black5),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16.0),
-                    child: Text(
                       'Sen, 17 Agu . 12 : 30',
                       style: HelperThemeData.textTheme.caption!
                           .copyWith(color: HelperColors.black5),
                     ),
-                  ),
-                  SizedBox(height: 6.0),
-                  LabelHalfCircular(
-                      text: text, backgroundColor: backgroundColor ?? HelperColors.green),
-                  SizedBox(height: 14.0),
-                ],
-              )
-            ],
+                    SizedBox(height: 6.0),
+                    HelperLabel(
+                        text: text, backgroundColor: backgroundColor ?? HelperColors.green),
+                    SizedBox(height: 14.0),
+                  ],
+                )
+              ],
+            ),
           ),
           Divider(
             thickness: 0.75,
