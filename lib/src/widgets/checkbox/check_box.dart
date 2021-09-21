d9aab6a19465d9f70137bdb0fc5fc986b06d47a5
Afdal <Afdaldev@gmail.com>
2021-09-21 15:08:08 +0800

    feat(check_box): change to dynamic

--- lib/src/widgets/checkbox/check_box.dart
+++ lib/src/widgets/checkbox/check_box.dart
@@ -1,75 +1,173 @@
 import 'package:flutter/material.dart';
 import 'package:helper_design/helper_design.dart';
 
-class HCheckBox extends StatelessWidget {
-  final String? text;
-  final bool value;
-  final ValueChanged<bool?> onChanged;
-  final double? radius;
-  final double? height;
+class HCheckBox<T> extends StatefulWidget {
+  const HCheckBox({
+    Key? key,
+    required this.value,
+    required this.selectedValue,
+    required this.onChanged,
+    this.size = 24,
+    this.withBorder = true,
+    this.controlPositionedTop,
+    this.controlPositionedBottom,
+    this.controlPositionedLeft,
+    this.controlPositionedRight,
+    this.width,
+    this.height,
+    this.child,
+    this.childPadding,
+  }) : super(key: key);
+  final T value;
+
+  final List<T> selectedValue;
+
+  final ValueChanged<T?> onChanged;
+
+  final double size;
+
+  final bool withBorder;
+
+  final double? controlPositionedTop;
+
+  final double? controlPositionedBottom;
+
+  final double? controlPositionedLeft;
+
+  final double? controlPositionedRight;
+
   final double? width;
-  final EdgeInsetsGeometry? padding;
-  final double? positionedTop;
-  final double? positionedBottom;
-  final double? positionedLeft;
-  final double? positionedRight;
-  final Widget child;
+
+  final double? height;
+
+  final Widget? child;
+
   final EdgeInsetsGeometry? childPadding;
 
-  const HCheckBox(
-      {Key? key,
-      required this.onChanged,
-      required this.value,
-      this.text,
-      this.radius,
-      this.height,
-      this.width,
-      this.padding,
-      this.positionedTop,
-      this.positionedBottom,
-      this.positionedLeft,
-      this.positionedRight,
-      required this.child,
-      this.childPadding})
-      : super(key: key);
+  bool get _selected => selectedValue.contains(value);
+
+  @override
+  _HCheckBoxState createState() => _HCheckBoxState<T>();
+}
+
+class _HCheckBoxState<T> extends State<HCheckBox<T>>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late List<Animation> _animations;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+
+    _animations = [
+      CurvedAnimation(parent: _controller, curve: Curves.ease),
+      ColorTween(
+        begin: HelperColors.black9,
+        end: HelperColors.orange,
+      ).animate(_controller),
+      ColorTween(
+        begin: HelperColors.black10,
+        end: HelperColors.orange,
+      ).animate(_controller),
+    ];
+
+    _animations.forEach((anim) => anim.addListener(() => setState(() {})));
+
+    if (widget._selected)
+      _controller.value = 1.0;
+    else
+      _controller.value = 0.0;
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HCheckBox<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if ((widget._selected &&
+            (_controller.status == AnimationStatus.dismissed ||
+                _controller.status == AnimationStatus.reverse)) ||
+        (!widget._selected &&
+            (_controller.status == AnimationStatus.completed ||
+                _controller.status == AnimationStatus.forward))) {
+      _updateState();
+    }
+  }
+
+  void _updateState() {
+    setState(() {
+      if (widget._selected && _controller.status != AnimationStatus.completed) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  void _handleChanged() {
+    widget.onChanged(widget.value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width ?? MediaQuery.of(context).size.width,
-      padding: padding,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius ?? 12),
-        border: Border.all(
-            width: 0.75,
-            color: value ? HelperColors.orange : HelperColors.black10),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            padding: childPadding,
-            child: child,
+    final int alpha = (_animations[0].value * 255).toInt();
+
+    final Widget control = InkWell(
+      onTap: _handleChanged,
+      child: AnimatedContainer(
+        duration: _controller.duration!,
+        curve: Curves.ease,
+        height: widget.size,
+        width: widget.size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: HelperColors.orange.withAlpha(alpha),
+          border: Border.all(
+            width: 1.0,
+            color: _animations[1].value,
           ),
-          Positioned(
-            top: positionedTop ?? 8,
-            bottom: positionedBottom,
-            left: positionedLeft,
-            right: positionedRight ?? 8,
-            child: SizedBox(
-              height: 24.0,
-              width: 24.0,
-              child: Checkbox(
-                value: value,
-                onChanged: onChanged,
-                shape: CircleBorder(),
-                activeColor: HelperColors.orange,
-                side: BorderSide(width: 1.0, color: HelperColors.black9),
-              ),
-            ),
-          ),
-        ],
+        ),
+        child: widget._selected
+            ? Icon(
+                Icons.check_rounded,
+                color: HelperColors.white,
+                size: widget.size - 4,
+              )
+            : SizedBox.shrink(),
       ),
     );
+
+    return widget.withBorder
+        ? Container(
+            height: widget.height,
+            width: widget.width ?? MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              border: Border.all(color: _animations[2].value, width: 0.75),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Stack(
+              children: [
+                Container(
+                  padding: widget.childPadding,
+                  child: widget.child,
+                ),
+                Positioned(
+                    top: widget.controlPositionedTop ?? 8,
+                    bottom: widget.controlPositionedBottom,
+                    left: widget.controlPositionedLeft,
+                    right: widget.controlPositionedRight ?? 8,
+                    child: control),
+              ],
+            ),
+          )
+        : control;
   }
 }
