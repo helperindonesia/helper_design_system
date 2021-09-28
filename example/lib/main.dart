1d6ea72a5547ef86061921ee32fa77c7d3f42083
Afdal <Afdaldev@gmail.com>
2021-09-28 16:42:42 +0800

    feat(example): try implement slider

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,12 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: HelperThemeData.themeData(),
-        home:
-        // OrderView()
-        MyHomePage(title: 'Helper Design Example'),
-        );
+      title: 'Flutter Demo',
+      theme: HelperThemeData.themeData(),
+      home:
+          // OrderView()
+          MyHomePage(title: 'Helper Design Example'),
+    );
   }
 }
 
@@ -34,6 +34,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int currentStep = 0;
+  double value = 25;
+  var values = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100];
 
   @override
   void initState() {
@@ -56,103 +58,65 @@ class _MyHomePageState extends State<MyHomePage> {
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
-        appBar: HelperAppBar.image(
-          name: 'Help Asisten',
-          mediaUrl:
-              'https://cdn1-production-images-kly.akamaized.net/WrP9G-ttMc51fEkHtJtDysZ5OY8=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2329745/original/020818800_1534239405-7._Allkpop.jpg',
-        ),
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-              width: MediaQuery.of(context).size.width,
-              child: StepView(
-                currentStep: currentStep,
-                stepViewType: StepViewType.horizontal,
-                steps: [
-                  ...List.generate(
-                    3,
-                    (i) => Step(
-                      title: 'Tahapan ${i + 1}',
-                      content: _buildContent(),
-                      action: PrimaryButton.icon(
-                        height: 48,
-                        text: 'Lanjutkan',
-                        onPressed: () {
-                          setState(() {
-                            if (currentStep < 2) currentStep++;
-                          });
-                        },
-                        icon: Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          appBar: HelperAppBar.image(
+            name: 'Help Asisten',
+            mediaUrl:
+                'https://cdn1-production-images-kly.akamaized.net/WrP9G-ttMc51fEkHtJtDysZ5OY8=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2329745/original/020818800_1534239405-7._Allkpop.jpg',
+          ),
+          body: Container(
+            child: HSlider(
+              minValue: 0,
+              maxValue: 100,
+              value: value,
+              majorTick: 6,
+              minorTick: 2,
+              labelValuePrecision: 0,
+              tickValuePrecision: 0,
+              onChanged: (val) => setState(() {
+                value = val;
+                print(value);
+              }),
+              activeColor: Colors.orange,
+              inactiveColor: Colors.orange.shade50,
+              groupValues: values,
             ),
-
-            Positioned(
-              child: DraggableBottomSheet(
-                initialChildSize: 0.3,
-                withHeader: true,
-                minChildSize: 0.3,
-                backgroundColor: HelperColors.orange10,
-                headerContent: Container(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Waktu Pengerjaan',
-                            style: HelperThemeData.textTheme.caption!
-                                .copyWith(color: HelperColors.black3),
-                          ),
-                          SizedBox(height: 2.0),
-                          Text(
-                            '01 : 10 : 30',
-                            style: HelperThemeData.textTheme.headline5!
-                                .copyWith(fontSize: 16.0),
-                          )
-                        ],
-                      ),
-                      HOutlinedButton.icon(
-                        outlineType: OutlineType.rounded,
-                        onPressed: () {},
-                        text: 'Tambah Waktu',
-                        icon: Icon(
-                          Icons.add_circle_rounded,
-                          size: 16.0,
-                          color: HelperColors.orange,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: HelperColors.white,
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(16.0),
-                      )),
-                  child: Column(
-                    children: _buildHelper(50),
-                  ),
-                ),
-              ),
-            ),
-
-
-          ],
-        ),
-      ),
+          )
+          // Stack(
+          //   fit: StackFit.expand,
+          //   children: [
+          //     Container(
+          //       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          //       width: MediaQuery.of(context).size.width,
+          //       child: StepView(
+          //         currentStep: currentStep,
+          //         stepViewType: StepViewType.horizontal,
+          //         steps: [
+          //           ...List.generate(
+          //             3,
+          //             (i) => Step(
+          //               title: 'Tahapan ${i + 1}',
+          //               content: _buildContent(),
+          //               action: PrimaryButton.icon(
+          //                 height: 48,
+          //                 text: 'Lanjutkan',
+          //                 onPressed: () {
+          //                   setState(() {
+          //                     if (currentStep < 2) currentStep++;
+          //                   });
+          //                 },
+          //                 icon: Icon(
+          //                   Icons.chevron_right_rounded,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          ),
     );
   }
 
@@ -272,8 +236,6 @@ class _MyHomePageState extends State<MyHomePage> {
                                           .copyWith(color: HelperColors.black3),
                                     ),
                                     SizedBox(height: 20.0),
-                                    PrimaryCard(onPressed: () {}, boxBorder: Border(),),
-                                    SizedBox(height: 20.0),
                                     PrimaryCard(
                                       title: 'Lihat Harga Dulu',
                                       description:
@@ -329,155 +291,24 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           );
         case 1:
-
           return Step(
-            content: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFieldWithContent(
-                  labelText: 'Ceritakan Kebutuhanmu',
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
-                      children: [
-//                         Icon(
-//                           Icons.edit_outlined,
-//                           size: 24.0,
-//                           color: HelperColors.black3,
-
-          return index == 0
-              ? Step(
-                  content: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      /* TextFieldWithContent(
-                        labelText: 'Ceritakan Kebutuhanmu',
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.edit_outlined,
-                                size: 24.0,
-                                color: HelperColors.black3,
-                              ),
-                              SizedBox(width: 12.0),
-                              Text(
-                                'Tulis dengan detail alamat',
-                                style: HelperThemeData.textTheme.caption!
-                                    .copyWith(color: HelperColors.black3),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),*/
-                      OutlineTextField(
-                        trailing: Align(
-                          alignment: Alignment.centerRight,
-                          child: HOutlinedButton(
-                            text: 'Peta',
-                            onPressed: () {},
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      TextFieldCounter(labelText: 'Jumlah Helpermu'),
-                      Container(
-                        decoration:
-                            ShapeDecoration(shape: ToolTipsShapeBorder()),
-                      )
-                    ],
-                  ),
-                )
-              : Step(
-                  content: CardContainer(
-                    padding: EdgeInsets.all(12.0),
-                    children: [
-                      Padding(
-                        padding:
-                            EdgeInsets.only(top: 1.0, left: 4.0, bottom: 2.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Ambil Barang di Kota",
-                              style: HelperThemeData.textTheme.buttonText1!
-                                  .copyWith(color: HelperColors.black),
-                            ),
-                            Icon(
-                              Icons.drag_handle,
-                              color: HelperColors.black8,
-                              size: 24.0,
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 4.0),
-                        child: Text(
-                          'Jln. Perintis Kemerdekaan',
-                          style: HelperThemeData.textTheme.caption!
-                              .copyWith(color: HelperColors.black5),
-                        ),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        HOutlinedButton.icon(
-                          borderColor: HelperColors.black5,
-                          onPressed: () {},
-                          text: 'Hapus',
-                          icon: Icon(
-                            Icons.remove_circle_rounded,
-                            size: 16.0,
-                            color: HelperColors.black3,
-                          ),
-                        ),
-                        HOutlinedButton.icon(
-                          borderColor: HelperColors.black5,
-                          onPressed: () {},
-                          text: 'Hapus',
-                          icon: Icon(
-                            Icons.edit,
-                            size: 16.0,
-                            color: HelperColors.black3,
-
-                      ),
-                      Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 4.0, vertical: 14.0),
-                          child:
-                              Divider() //TODO: Change Divider to DashLine after Merge
-                          ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          HOutlinedButton.icon(
-                            borderColor: HelperColors.black5,
-                            onPressed: () {},
-                            text: 'Hapus',
-                            textColor: HelperColors.black5,
-                            icon: Icon(
-                              Icons.remove_circle_rounded,
-                              size: 16.0,
-                              color: HelperColors.black3,
-                            ),
-
-                          ),
-                          HOutlinedButton.icon(
-                            // borderColor: HelperColors.black5,
-                            onPressed: () {},
-                            text: 'Edit',
-                            icon: Icon(
-                              Icons.edit,
-                              size: 16.0,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
+              content: Column(
+            children: [
+              Slider(
+                min: 0,
+                max: 100,
+                value: value,
+                onChanged: (val) {
+                  setState(() {
+                    value = val;
+                  });
+                },
+                divisions: value >= 50 ? 10 : 20,
+                label: value.toString(),
+              ),
+              // HSlider()
+            ],
+          ));
         case 2:
           return Step(
             content: Column(
