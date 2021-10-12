7dceba395c5bb929849b90ecdba550252258a10b
Afdal <Afdaldev@gmail.com>
2021-10-12 16:35:34 +0800

    feat(tool_tips_shape_border): add left and right ToolTipTickPosition

--- lib/src/widgets/tooltips/tool_tips_shape_border.dart
+++ lib/src/widgets/tooltips/tool_tips_shape_border.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
+enum ToolTipTickPosition { LEFT, RIGHT }
+
 class ToolTipsShapeBorder extends ShapeBorder {
   final double arrowWidth;
   final double arrowHeight;
   final double arrowArc;
   final double radius;
   final double padding;
+  final ToolTipTickPosition toolTipTickPosition;
 
   ToolTipsShapeBorder(
       {this.radius = 0,
       this.arrowWidth = 16.0,
       this.arrowHeight = 8.0,
       this.arrowArc = 0.5,
-      this.padding = 49.0})
+      this.padding = 49.0,
+      this.toolTipTickPosition = ToolTipTickPosition.RIGHT})
       : assert(arrowArc <= 1.0 && arrowArc >= 0.0);
 
   @override
@@ -29,7 +33,13 @@ class ToolTipsShapeBorder extends ShapeBorder {
     double x = arrowWidth, y = arrowHeight, r = 1 - arrowArc;
     return Path()
       ..addRRect(RRect.fromRectAndRadius(rect, Radius.circular(radius)))
-      ..moveTo(rect.topRight.dx - padding, rect.topRight.dy)
+      ..moveTo(
+          toolTipTickPosition == ToolTipTickPosition.RIGHT
+              ? rect.topRight.dx - padding
+              : rect.topLeft.dx + padding,
+          toolTipTickPosition == ToolTipTickPosition.RIGHT
+              ? rect.topRight.dy
+              : rect.topLeft.dy)
       ..relativeLineTo(-x / 2 * r, -y * r)
       ..relativeQuadraticBezierTo(
           -x / 2 * (1 - r), -y * (1 - r), -x * (1 - r), 0)
