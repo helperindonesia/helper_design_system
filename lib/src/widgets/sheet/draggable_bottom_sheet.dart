85ca3f36fd21965cae8b123fd5c4d359488679b7
Fordyta Abubakar <dhytodev@gmail.com>
2022-02-24 06:29:01 +0800

    feat(draggable_bottom_sheet): add ExtentListenerCallback

--- lib/src/widgets/sheet/draggable_bottom_sheet.dart
+++ lib/src/widgets/sheet/draggable_bottom_sheet.dart
@@ -1,85 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:helper_design/helper_design.dart';
 
+typedef ExtentListener = Function(double extent);
+
 class DraggableBottomSheet extends StatelessWidget {
   final Widget? child;
-  final double? radius;
-  final Color? childBackgroundColor;
-  final double? initialChildSize;
-  final double? minChildSize;
-  final double? maxChildSize;
+  final double radius;
+  final Color childBackgroundColor;
+  final double initialChildSize;
+  final double minChildSize;
+  final double maxChildSize;
   final bool withHeader;
   final Widget? headerContent;
-  final Color? indicatorColor;
-  final Color? headerBackgroundColor;
+  final Color indicatorColor;
+  final Color headerBackgroundColor;
+  final ExtentListener? extentListener;
 
-  const DraggableBottomSheet(
-      {Key? key,
-      required this.child,
-      this.radius,
-      this.childBackgroundColor,
-      this.initialChildSize,
-      this.minChildSize,
-      this.maxChildSize,
-      this.withHeader = false,
-      this.headerContent,
-      this.indicatorColor,
-      this.headerBackgroundColor})
-      : super(key: key);
+  const DraggableBottomSheet({
+    Key? key,
+    required this.child,
+    this.radius = 16,
+    this.childBackgroundColor = HelperColors.white,
+    this.initialChildSize = 0.2,
+    this.minChildSize = 0.1,
+    this.maxChildSize = 1.0,
+    this.withHeader = false,
+    this.headerContent,
+    this.indicatorColor = HelperColors.orange8,
+    this.headerBackgroundColor = HelperColors.white,
+    this.extentListener,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: initialChildSize ?? 0.2,
-      minChildSize: minChildSize ?? 0.1,
-      maxChildSize: maxChildSize ?? 1.0,
-      builder: (BuildContext context, ScrollController scrollController) {
-        return Container(
-          decoration: BoxDecoration(
-            color: headerBackgroundColor ?? HelperColors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(radius ?? 16.0),
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 13.0, bottom: 11.0),
-                height: 2.0,
-                width: 42.0,
-                decoration: BoxDecoration(
-                    color: indicatorColor ?? HelperColors.orange8,
-                    borderRadius: BorderRadius.circular(50.0)),
+    return NotificationListener<DraggableScrollableNotification>(
+      onNotification: (notification) {
+        extentListener!(notification.extent);
+
+        return true;
+      },
+      child: DraggableScrollableSheet(
+        initialChildSize: initialChildSize,
+        minChildSize: minChildSize,
+        maxChildSize: maxChildSize,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              color: headerBackgroundColor,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(radius),
               ),
-              withHeader
-                  ? SingleChildScrollView(
-                      controller: scrollController,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        color: headerBackgroundColor ?? HelperColors.white,
-                        child: headerContent,
-                      ),
-                    )
-                  : SizedBox(),
-              Expanded(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 13.0, bottom: 11.0),
+                  height: 2.0,
+                  width: 42.0,
                   decoration: BoxDecoration(
-                      color: childBackgroundColor ?? HelperColors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16))),
-                  child: SingleChildScrollView(
-                    controller: scrollController,
-                    child: child,
+                    color: indicatorColor,
+                    borderRadius: BorderRadius.circular(50.0),
                   ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+                withHeader
+                    ? SingleChildScrollView(
+                        controller: scrollController,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          color: headerBackgroundColor,
+                          child: headerContent,
+                        ),
+                      )
+                    : SizedBox(),
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: childBackgroundColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16),
+                      ),
+                    ),
+                    child: SingleChildScrollView(
+                      controller: scrollController,
+                      child: child,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
