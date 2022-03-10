4e6d6a7e5624400b7d057666bb41a224168dcf55
Fordyta Abubakar <dhytodev@gmail.com>
2022-03-10 19:04:41 +0800

    feat(bottom_sheet): improve and clean up the ModalBottomSheet

--- lib/src/widgets/sheet/modal_bottom_sheet.dart
+++ lib/src/widgets/sheet/modal_bottom_sheet.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:helper_design/helper_design.dart';
 
 class ModalBottomSheet extends StatelessWidget {
-  final Color? backgroundColor;
+  final Color backgroundColor;
   final List<Widget> children;
-  final MainAxisAlignment? mainAxisAlignment;
-  final CrossAxisAlignment? crossAxisAlignment;
-  final IconData? rightIcon;
-  final IconData? leftIcon;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final IconData rightIcon;
+  final IconData leftIcon;
   final VoidCallback? onRightButtonPressed;
   final VoidCallback? onLeftButtonPressed;
   final EdgeInsetsGeometry? contentPadding;
 
   const ModalBottomSheet({
     Key? key,
-    this.backgroundColor,
+    this.backgroundColor = HelperColors.white,
     required this.children,
-    this.mainAxisAlignment,
-    this.crossAxisAlignment,
-    this.rightIcon,
-    this.leftIcon,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.rightIcon = Icons.close_rounded,
+    this.leftIcon = Icons.arrow_back,
     this.onRightButtonPressed,
     this.onLeftButtonPressed,
     this.contentPadding = const EdgeInsets.all(16),
@@ -28,65 +28,57 @@ class ModalBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Wrap(
+      spacing: 1,
       children: [
-        Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: onLeftButtonPressed != null
-                    ? MainAxisAlignment.spaceBetween
-                    : MainAxisAlignment.end,
-                children: [
-                  onLeftButtonPressed != null
-                      ? BaseButton(
-                          onPressed: onLeftButtonPressed,
-                          backgroundColor:
-                              backgroundColor ?? HelperColors.white,
-                          height: 40.0,
-                          width: 40.0,
-                          child: Icon(
-                            leftIcon ?? Icons.arrow_back,
-                            size: 24.0,
-                            color: HelperColors.black3,
-                          ),
-                        )
-                      : SizedBox(),
-                  onRightButtonPressed != null
-                      ? BaseButton(
-                          onPressed: onRightButtonPressed,
-                          backgroundColor:
-                              backgroundColor ?? HelperColors.white,
-                          height: 40.0,
-                          width: 40.0,
-                          child: Icon(
-                            rightIcon ?? Icons.close_rounded,
-                            size: 24.0,
-                            color: HelperColors.black3,
-                          ),
-                        )
-                      : SizedBox(),
-                ],
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              padding: contentPadding,
-              decoration: BoxDecoration(
-                color: backgroundColor ?? HelperColors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(16.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: onLeftButtonPressed != null
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.end,
+            children: [
+              if (onLeftButtonPressed != null)
+                BaseButton(
+                  onPressed: onLeftButtonPressed,
+                  backgroundColor: backgroundColor,
+                  height: 40.0,
+                  width: 40.0,
+                  child: Icon(
+                    leftIcon,
+                    size: 24.0,
+                    color: HelperColors.black3,
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.start,
-                crossAxisAlignment:
-                    crossAxisAlignment ?? CrossAxisAlignment.start,
-                children: children,
-              ),
+              if (onRightButtonPressed != null)
+                BaseButton(
+                  onPressed: onRightButtonPressed,
+                  backgroundColor: backgroundColor,
+                  height: 40.0,
+                  width: 40.0,
+                  child: Icon(
+                    rightIcon,
+                    size: 24.0,
+                    color: HelperColors.black3,
+                  ),
+                ),
+            ],
+          ),
+        ),
+        SizedBox(height: 16.0),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          padding: contentPadding,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(16.0),
             ),
-          ],
+          ),
+          child: Column(
+            mainAxisAlignment: mainAxisAlignment,
+            crossAxisAlignment: crossAxisAlignment,
+            children: children,
+          ),
         ),
       ],
     );
